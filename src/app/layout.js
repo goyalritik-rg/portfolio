f64937f042a11fcb468f992774b423e32485614d
Ritik Goyal <ritik.goyal@cogoport.com>
2025-04-11 18:09:05 +0530

    insights added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
