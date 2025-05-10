d4a61cbbef9a73ad523fe07b46faec05095826cd
Ritik Goyal <goyalritik555.rg@gmail.com>
2025-05-10 16:09:42 +0530

    meta tags added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -21,7 +22,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        {/* Ensure consistent text rendering across browsers */}
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
       <body className={`${poppins.variable} antialiased`}>
         {children}
         <Analytics />
