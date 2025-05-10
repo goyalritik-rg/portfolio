5ae632b76c832bc7232e135804ee3a07e24d52f7
Ritik Goyal <goyalritik555.rg@gmail.com>
2025-05-11 03:21:19 +0530

    button css changes

--- src/common/ui-components/Button.js
+++ src/common/ui-components/Button.js
@@ -7,7 +7,8 @@ import Loader from "./Loader";
 const typeClasses = {
   primary:
     "bg-lime-400 text-neutral-950 border-lime-400 whitespace-nowrap hover:bg-white hover:border-white",
-  secondary: "border-white text-white bg-transparent hover:bg-neutral-800",
+  secondary:
+    "border-white text-white bg-transparent hover:text-lime-400 hover:border-lime-400",
 };
 
 const loaderClasses = {
@@ -31,7 +32,7 @@ const Button = ({
     <motion.button
       whileTap={{ scale: 0.95 }}
       className={twMerge(
-        "border h-12 rounded-full px-6 font-medium cursor-pointer relative transition-all",
+        "border h-12 rounded-full px-6 font-medium cursor-pointer relative transition-all duration-300 ease-in-out",
         typeClasses[type],
         sizeClasses[size],
         className
