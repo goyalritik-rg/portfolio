39e1bd4d967e88b42deb785b40af63466fe97621
Ritik Goyal <goyalritik555.rg@gmail.com>
2025-05-17 18:25:20 +0530

    cta mouse pointer added

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,17 +1,14 @@
 "use client";
 
-import CustomCursor from "@/common/components/CustomCursor";
 import { EMAIL } from "@/constants";
-import { motion, useAnimate } from "framer-motion";
-import Link from "next/link";
+import { motion, useAnimate, AnimatePresence } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 const CallToAction = () => {
   const ref = useRef();
-
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [hovered, setHovered] = useState(false);
-
   const [scope, animate] = useAnimate();
 
   useEffect(() => {
@@ -28,42 +25,65 @@ const CallToAction = () => {
     }
   }, [hovered]);
 
-  return (
-    <section className="py-24 w-[100vw]">
-      <div className="overflow-x-clip p-4 flex">
-        <CustomCursor showCustomCursor={hovered} />
+  const handleMouseMove = (e) => {
+    setMousePosition({ x: e.clientX, y: e.clientY });
+  };
 
-        <Link
-          href={`mailto:${EMAIL}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex flex-none  cursor-pointer"
+  return (
+    <section className="py-24 w-[100vw] relative" onMouseMove={handleMouseMove}>
+      <div
+        className="overflow-x-clip p-4 flex"
+        role="presentation"
+        onClick={() => {
+          window.open(`mailto:${EMAIL}`, "_blank");
+        }}
+      >
+        <motion.div
+          ref={scope}
+          className="flex flex-none gap-16 pr-16 text-6xl md:text-[80px] font-medium"
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
         >
-          <motion.div
-            ref={scope}
-            className="flex flex-none gap-16 pr-16 text-6xl md:text-[80px] font-medium cursor-pointer"
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
-            style={{ cursor: hovered ? "none" : "pointer" }}
-          >
-            {Array.from({ length: 10 }).map((_, i) => {
-              return (
-                <div key={i} className="flex items-center gap-16">
-                  <span className="text-lime-400 text-7xl">&#10038;</span>
-                  <span
-                    className={twMerge(
-                      "transition-all",
-                      hovered && "text-lime-300"
-                    )}
-                  >
-                    Let&apos;s Connect
-                  </span>
-                </div>
-              );
-            })}
-          </motion.div>
-        </Link>
+          {Array.from({ length: 10 }).map((_, i) => {
+            return (
+              <div key={i} className="flex items-center gap-16">
+                <span className="text-lime-400 text-7xl">&#10038;</span>
+                <span
+                  className={twMerge(
+                    "transition-all",
+                    hovered && "text-lime-300"
+                  )}
+                >
+                  Let&apos;s Connect
+                </span>
+              </div>
+            );
+          })}
+        </motion.div>
       </div>
+
+      <AnimatePresence>
+        {hovered && (
+          <motion.div
+            className="fixed px-3 py-1 w-fit rounded-full font-medium bg-purple-600 text-foreground text-sm"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            transition={{
+              type: "spring",
+              stiffness: 500,
+              damping: 25,
+              mass: 0.5,
+            }}
+            style={{
+              left: mousePosition.x + 25,
+              top: mousePosition.y + 15,
+            }}
+          >
+            Yes, please 🙌
+          </motion.div>
+        )}
+      </AnimatePresence>
     </section>
   );
 };
