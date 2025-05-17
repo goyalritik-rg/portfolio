2518af401ab07b9bd189af61c07fdbd01e05b403
Ritik Goyal <goyalritik555.rg@gmail.com>
2025-05-17 17:17:57 +0530

    email added to cta

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,7 +1,9 @@
 "use client";
 
 import CustomCursor from "@/common/components/CustomCursor";
+import { EMAIL } from "@/constants";
 import { motion, useAnimate } from "framer-motion";
+import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
@@ -31,29 +33,36 @@ const CallToAction = () => {
       <div className="overflow-x-clip p-4 flex">
         <CustomCursor showCustomCursor={hovered} />
 
-        <motion.div
-          ref={scope}
-          className="flex flex-none gap-16 pr-16 text-6xl md:text-[80px] font-medium cursor-pointer"
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-          style={{ cursor: hovered ? "none" : "pointer" }}
+        <Link
+          href={`mailto:${EMAIL}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex flex-none  cursor-pointer"
         >
-          {Array.from({ length: 10 }).map((_, i) => {
-            return (
-              <div key={i} className="flex items-center gap-16">
-                <span className="text-lime-400 text-7xl">&#10038;</span>
-                <span
-                  className={twMerge(
-                    "transition-all",
-                    hovered && "text-lime-300"
-                  )}
-                >
-                  Let&apos;s Connect
-                </span>
-              </div>
-            );
-          })}
-        </motion.div>
+          <motion.div
+            ref={scope}
+            className="flex flex-none gap-16 pr-16 text-6xl md:text-[80px] font-medium cursor-pointer"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            style={{ cursor: hovered ? "none" : "pointer" }}
+          >
+            {Array.from({ length: 10 }).map((_, i) => {
+              return (
+                <div key={i} className="flex items-center gap-16">
+                  <span className="text-lime-400 text-7xl">&#10038;</span>
+                  <span
+                    className={twMerge(
+                      "transition-all",
+                      hovered && "text-lime-300"
+                    )}
+                  >
+                    Let&apos;s Connect
+                  </span>
+                </div>
+              );
+            })}
+          </motion.div>
+        </Link>
       </div>
     </section>
   );
